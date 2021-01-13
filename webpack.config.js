bc02e0f19625105621b492a1359c7baba61bc378
Konstantin Lyakh <kostya.lyakh@gmail.com>
2021-01-14 01:07:42 +0200

    Initial template

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+module.exports = [{
+  entry: ['./app.scss', './app.js'],
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              }
+            },
+          },
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ]
+  },
+}];
